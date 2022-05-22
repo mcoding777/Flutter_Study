807aecafdcc44b1bfef9d5f0511df3f321410c7e
misun <mcoding777@naver.com>
2022-05-22 17:20:24 +0900

    feat: input controller 연결

--- bmi/lib/input.dart
+++ bmi/lib/input.dart
@@ -11,6 +11,17 @@ class _BmiMainState extends State<BmiMain> {
   final _formKey = GlobalKey<FormState>(); // 폼의 상태를 얻기 위한 키
   // GlobalKey : 요소를 식별함, 다른 키로 접근할 수 있는 방법을 제공, 하위 위젯에도 적용됨
 
+  final _heightController = TextEditingController(); // 인풋 값을 가져올 컨트롤러 객체
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    // 화면이 종료될 때 컨트롤러 해제
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +40,14 @@ class _BmiMainState extends State<BmiMain> {
                     hintText: '키', // placeholder는 키
                   ),
                   keyboardType: TextInputType.number, // 숫자만 입력 가능함
+                  controller: _heightController, // 컨트롤러 연결
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      // 입력한 값의 앞뒤 공백을 제외한 값이 비어있다면
+                      return '키를 입력하세요';
+                    }
+                    return null; // 에러가 없으면 null 반환
+                  },
                 ),
                 const SizedBox(
                   height: 16.0, // input 요소 사이에 여백 제공
@@ -39,6 +58,13 @@ class _BmiMainState extends State<BmiMain> {
                     hintText: '몸무게',
                   ),
                   keyboardType: TextInputType.number,
+                  controller: _weightController,
+                  validator: (value) {
+                    if (value!.trim().isEmpty) {
+                      return '몸무게를 입력하세요';
+                    }
+                    return null;
+                  },
                 ),
                 Container(
                   // 결과 버튼이 들어갈 컨테이너

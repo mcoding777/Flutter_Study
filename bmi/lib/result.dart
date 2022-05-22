4f3b59b08d218776f7e38edd663f903e968f6aa0
misun <mcoding777@naver.com>
2022-05-22 17:51:04 +0900

    feat: 계산한 bmi 값에 따라 결과 값 다르게 반환

--- bmi/lib/result.dart
+++ bmi/lib/result.dart
@@ -9,26 +9,64 @@ class BmiResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bmi = weight / ((height / 100) * (height / 100));
+
+    String _calcBmi(double bmi) {
+      var result = '저체중';
+
+      if (bmi >= 35) {
+        result = '고도 비만';
+      } else if (bmi >= 30) {
+        result = '2단계 비만';
+      } else if (bmi >= 25) {
+        result = '1단계 비만';
+      } else if (bmi >= 23) {
+        result = '과체중';
+      } else if (bmi >= 18.5) {
+        result = '정상';
+      }
+      return result;
+    }
+
+    Widget _buildIcon(double bmi) {
+      if (bmi >= 23) {
+        return const Icon(
+          Icons.sentiment_very_dissatisfied,
+          color: Colors.red,
+          size: 100,
+        );
+      } else if (bmi >= 18.5) {
+        return const Icon(
+          Icons.sentiment_satisfied,
+          color: Colors.green,
+          size: 100,
+        );
+      } else {
+        return const Icon(
+          Icons.sentiment_dissatisfied,
+          color: Colors.orange,
+          size: 100,
+        );
+      }
+    }
+
     return Scaffold(
         appBar: AppBar(title: const Text('비만도 계산기')),
         body: Center(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
+              children: <Widget>[
                 Text(
-                  '정상',
-                  style: TextStyle(
+                  _calcBmi(
+                      bmi), // 에러 발생 : 'Null' 유형의 값은 const 생성자에서 'String' 유형의 매개변수에 할당할 수 없습니다. 하위 유형을 사용하거나 'const' 키워드를 제거해 보십시오.
+                  style: const TextStyle(
                     fontSize: 36,
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 16,
                 ),
-                Icon(
-                  Icons.sentiment_satisfied,
-                  color: Colors.green,
-                  size: 100,
-                )
+                _buildIcon(bmi),
               ]),
         ));
   }

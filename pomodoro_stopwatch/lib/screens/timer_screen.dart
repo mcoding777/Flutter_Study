5f864121b3b6fbce6638320f517ffa9c226dc8f7
misun <mcoding777@naver.com>
2022-06-25 23:41:24 +0900

    feat: 함수 적용

--- pomodoro_stopwatch/lib/screens/timer_screen.dart
+++ pomodoro_stopwatch/lib/screens/timer_screen.dart
@@ -117,15 +117,19 @@ class _TimerScreenState extends State<TimerScreen> {
     // 러닝 상태일 때 버튼 위젯
     final List<Widget> _runningButtons = [
       ElevatedButton(
-        onPressed: () {},
+        onPressed: () {
+          _timerStatus == TimerStatus.paused ? resume : pause;
+        },
         style: ElevatedButton.styleFrom(primary: Colors.blue),
-        child: const Text(
-            1 == 2 ? '계속하기' : '일시정지', // 중단된 상태인가 ? true 계속하기 : false 일시정지
-            style: TextStyle(color: Colors.white, fontSize: 16)),
+        child: Text(
+            _timerStatus == TimerStatus.paused
+                ? '계속하기'
+                : '일시정지', // 중단된 상태인가 ? true 계속하기 : false 일시정지
+            style: const TextStyle(color: Colors.white, fontSize: 16)),
       ),
       const Padding(padding: EdgeInsets.all(20)),
       ElevatedButton(
-          onPressed: () {},
+          onPressed: stop,
           style: ElevatedButton.styleFrom(primary: Colors.grey),
           child: const Text('포기하기', style: TextStyle(fontSize: 16)))
     ];
@@ -133,15 +137,16 @@ class _TimerScreenState extends State<TimerScreen> {
     // 정지 상태일 때 버튼 위젯
     final List<Widget> _stoppedButtons = [
       ElevatedButton(
-        onPressed: () {},
+        onPressed: run,
         style: ElevatedButton.styleFrom(
-            primary: 1 == 2
+            primary: _timerStatus == TimerStatus.resting
                 ? Colors.green
                 : Colors.blue), // 휴식 중인가? true 초록색 : false 파란색
         child: const Text('시작하기',
             style: TextStyle(color: Colors.white, fontSize: 16)),
       ),
     ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('뽀모도로 타이머'),
@@ -152,16 +157,16 @@ class _TimerScreenState extends State<TimerScreen> {
           Container(
             height: MediaQuery.of(context).size.height * 0.5,
             width: MediaQuery.of(context).size.height * 0.6,
-            decoration: const BoxDecoration(
+            decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                color: 1 == 2
+                color: _timerStatus == TimerStatus.resting
                     ? Colors.green
                     : Colors.blue // 휴식 중인가? true 초록색 : false 파란색
                 ),
-            child: const Center(
+            child: Center(
               child: Text(
-                '00:00',
-                style: TextStyle(
+                secondsToString(_timer),
+                style: const TextStyle(
                   color: Colors.white,
                   fontSize: 48,
                   fontWeight: FontWeight.bold,

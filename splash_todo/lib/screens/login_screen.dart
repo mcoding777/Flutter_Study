93ab7ce92e3dfffa06f0f3f3a67ff25bb322e064
misun <mcoding777@naver.com>
2022-07-03 22:53:56 +0900

    feat: 가짜 로그인 기능 구현

--- splash_todo/lib/screens/login_screen.dart
+++ splash_todo/lib/screens/login_screen.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'list_screen.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  const LoginScreen(
+      {super.key}); // const LoginScreen({Key? key}) : super(key: key); 와 동일함
+
+  // isLogin 값을 true로 변경
+  Future setLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isLogin', true);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('로그인'),
+      ),
       body: Center(
-        child: Text('LoginScreen'),
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          width: MediaQuery.of(context).size.width * 0.85,
+          child: ElevatedButton(
+            onPressed: () {
+              setLogin().then((_) {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const ListScreen()));
+              });
+            },
+            child: const Text("로그인"),
+          ),
+        ),
       ),
     );
   }

fca46d7dfb39160dd7dd047393156e7902cb92f2
misun <mcoding777@naver.com>
2022-07-10 19:37:07 +0900

    feat: const 추가

--- bloc_counter/lib/blocs/counter/bloc/counter_bloc.dart
+++ bloc_counter/lib/blocs/counter/bloc/counter_bloc.dart
@@ -8,7 +8,7 @@ part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(CounterState(0));
+  CounterBloc() : super(const CounterState(0));
 
   @override
   Stream<CounterState> mapEventToState(
